a2d4943c76a8ebb7d1b8acedfa3d9a23b10678c4
ALee008 <nomansland008@googlemail.com>
2021-07-19 22:21:04 +0200

    refactored sync_sftp.py; added requirements.txt

--- sync_sftp.py
+++ sync_sftp.py
@@ -1,15 +1,18 @@
-import paramiko
+import os
+import logging
 
-remote_ip = 'test.rebex.net'
-remote_port = 22
-user = "demo"
-password = "password"
-download_path = "./data/imap-console-client.png"
-remote_path = "./pub/example/imap-console-client.png"
+import paramiko.util
+import local_settings as settings
 
+# paramiko has a named logger. Use it.
+logging.basicConfig()
+logging.getLogger('paramiko').setLevel(logging.DEBUG)
+paramiko.util.log_to_file(settings.paths['logs'])
 # create sftp object
-transport = paramiko.Transport((remote_ip, remote_port))
-transport.connect(username=user, password=password)
-sftp = paramiko.SFTPClient.from_transport(transport)
-sftp.get(remote_path, download_path)
-transport.close()
+with paramiko.Transport((settings.server['server'], settings.server['port'])) as transport:
+    transport.connect(username=settings.login['user'], password=settings.login['password'])
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    for file_name in settings.files:
+        source = os.path.join(settings.paths['source'], file_name)
+        destination = os.path.join(settings.paths['destination'], file_name)
+        sftp.get(source, destination)
