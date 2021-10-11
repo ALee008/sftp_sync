ed840d329620badd609e0830db5b535896f63806
ALee008 <nomansland008@googlemail.com>
2021-10-11 13:17:07 +0200

    added socket.timeout exception to ignore this error and log a warning

--- sync_sftp.py
+++ sync_sftp.py
@@ -5,6 +5,7 @@ import sys
 import tqdm
 import paramiko.util
 import schedule
+import socket
 
 import local_settings as settings
 
@@ -16,13 +17,17 @@ paramiko.util.log_to_file(settings.paths['logs'])
 
 def download_files():
     # create sftp object
-    with paramiko.Transport((settings.server['server'], settings.server['port'])) as transport:
-        transport.connect(username=settings.login['user'], password=settings.login['password'])
-        sftp = paramiko.SFTPClient.from_transport(transport)
-        for file_name in tqdm.tqdm(settings.files, file=sys.stdout):
-            source = os.path.join(settings.paths['source'], file_name)
-            destination = os.path.join(settings.paths['destination'], file_name)
-            sftp.get(source, destination)
+    try:
+        with paramiko.Transport((settings.server['server'], settings.server['port'])) as transport:
+            transport.connect(username=settings.login['user'], password=settings.login['password'])
+            sftp = paramiko.SFTPClient.from_transport(transport)
+            for file_name in tqdm.tqdm(settings.files, file=sys.stdout):
+                source = os.path.join(settings.paths['source'], file_name)
+                destination = os.path.join(settings.paths['destination'], file_name)
+                sftp.get(source, destination)
+    except socket.timeout as msg:
+        logging.WARNING(f"A timeout was ignored: Actual exception message: {msg}")
+        pass
 
     return None
 
